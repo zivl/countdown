8d6d0eebc9362b0c9e35513095f7a017e7f96c89
Ziv Levy <levi.ziv@gmail.com>
2023-04-14 10:01:32 +0300

    last deployment

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/countdown/__docusaurus/debug/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/', '23b'),
-    exact: true
-  },
-  {
-    path: '/countdown/__docusaurus/debug/config/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/config/', 'e03'),
-    exact: true
-  },
-  {
-    path: '/countdown/__docusaurus/debug/content/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/content/', '652'),
-    exact: true
-  },
-  {
-    path: '/countdown/__docusaurus/debug/globalData/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/globalData/', '65e'),
-    exact: true
-  },
-  {
-    path: '/countdown/__docusaurus/debug/metadata/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/metadata/', '021'),
-    exact: true
-  },
-  {
-    path: '/countdown/__docusaurus/debug/registry/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/registry/', 'bf8'),
-    exact: true
-  },
-  {
-    path: '/countdown/__docusaurus/debug/routes/',
-    component: ComponentCreator('/countdown/__docusaurus/debug/routes/', '5a9'),
-    exact: true
-  },
-  {
     path: '/countdown/docs/',
     component: ComponentCreator('/countdown/docs/', 'f73'),
     routes: [
